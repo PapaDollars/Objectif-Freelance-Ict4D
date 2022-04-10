23a94141e380703aa6405952b7f0496b676e9563
daniel iya <iyadaniel51@gmail.com>
2022-04-10 03:47:47 -0100

    depot des projets courants

---
+++ html/proget/css/APK.cs
@@ -0,0 +1,21 @@
+#nav_footer_1{
+    text-align: center;
+    margin: 6% 5% 0% 5%;
+    background-color: cornsilk;
+}
+#nav_footer_2{
+    text-align: center;
+    margin: 6% 5% 0% 5%;
+    background-color: cornsilk;
+}
+#nav_footer_3{
+    text-align: center;
+    margin: 6% 5% 0% 5%;
+    background-color: cornsilk;
+}
+#nav_footer_4{
+    text-align: center;
+    margin: 6% 5% 0% 5%;
+    background-color: cornsilk;
+}
+//---------------------------------------------------------
---
+++ html/proget/css/style.cs
@@ -0,0 +1,98 @@
+#tete
+{
+     display: flex;
+     width: 80%;
+     height: 15%;
+     margin: 0% 5% 0% 5%;
+     padding: 2% 5% 2% 5%;
+ }
+#menu_1{
+    text-align: center;
+    text-color: blue;
+    margin: 6% 5% 0% 5%;
+}
+#menu_2{
+    text-align: center;
+    text-color: blue;
+    margin: 6% 5% 0% 5%;
+}
+#menu_3{
+    text-align: center;
+    text-color: blue;
+    margin: 6% 5% 0% 5%;
+}
+#menu_4{
+    text-align: center;
+    text-color: blue;
+    margin: 6% 5% 0% 5%;
+}
+#image{
+    margin: -1% 5% 0% -10%;
+}
+#image_1{
+    background-image: url(image/image.jpg) no-repeat;
+    background-size: cover;
+}
+
+#titre
+{    text-color: green;
+    margin:150px 50px 50px 110px;
+    font-family: Cooper;
+    font-size: 60px;
+}
+#titre:hover
+{
+    color: yellow;
+}
+//------------------------------------------------------
+
+#h2
+{
+    margin-left:100px;
+     font-size:25px;
+     color:white;
+}
+
+#bla
+{
+    widht: 10px;
+    margin:50px 1px 50px 500px;
+    color:white;
+    background-color: black;
+}
+
+#a1:hover
+{
+    color: red;
+}
+#a1
+{
+    margin:200px 50px 50px 300px;
+    padding:0px 20px 0px 20px;
+    font-size:20px;
+    border:1px solid yellow;
+    box-shadow: 16px 6px 0px black;
+    background-color: yellow;
+    text-decoration: none;
+    color:black;
+}
+
+#table{
+    margin : 100px 50px 10px 0px;
+}
+//------------------------------------------------------
+#footer{
+    margin: 300px 300px 200px 500px;
+}
+a
+{
+    text-decoration: none;
+}
+
+li
+{
+    list-style-type:none;
+    margin-left: 90px;
+}
+
+//----------------------------------------------------------------
